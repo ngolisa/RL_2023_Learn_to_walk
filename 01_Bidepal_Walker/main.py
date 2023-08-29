4a9c5b9b3837ffc66d284385df54af21529ad4a1
Paul Chupot <paul.chupot@edu.escp.eu>
2023-08-29 15:32:02 +0200

    le modèle commence à apprendre du moins à chaque fois que nous lançons le main

--- 01_Bidepal_Walker/main.py
+++ 01_Bidepal_Walker/main.py
@@ -1,17 +1,54 @@
 import gym
 import torch
-
+import numpy as np
 import pickle
-
+from agent import DQNAgent
+from buffer import BUF
+from config import CFG
 # import data
 # train model
 # evaluate model
 
 
 
-env = gym.make("BipedalWalker-v3",hardcore=True)
+env = gym.make("BipedalWalker-v3",hardcore=True, render_mode='human')
 
 iterations = 1000
+
+#print(env.observation_space.shape[0], env.action_space.shape[0])
+
+agent = DQNAgent(env.observation_space.shape[0], env.action_space.shape[0])
+
+terminated = True
+for _ in range(iterations):
+
+    if terminated :
+        obs_old, info = env.reset()
+
+    action = agent.get(obs_old, env.action_space)
+
+    obs_new, reward, terminated, truncated, info = env.step(action)
+
+    BUF.set((obs_old, action, reward, obs_new))
+
+    try :
+        old_list, act_list, rwd_list, new_list = BUF.get()
+        for i in range(CFG.batch_size):
+            agent.set(old_list[i], act_list[i], rwd_list[i], new_list[i])
+            print(f'training {i} of step{_}completed')
+
+    except :
+        agent.set(obs_old, action, reward, obs_new)
+
+    obs_old = obs_new
+
+    #print(BUF.len())
+
+
+
+
+
+'''
 with open('01_Bidepal_Walker/data/data.pickle', 'wb') as f:
     for i in range(iterations):
         print(f"{i}/{iterations} ie {round(i/iterations,2)*100}%")
@@ -40,3 +77,4 @@ print(len(objects))
 
 rewards = [i[3] for i in objects]
 print(max(rewards))
+'''
--- 01_Bidepal_Walker/model.py
+++ 01_Bidepal_Walker/model.py
@@ -1,9 +1,19 @@
+import torch
 import torch.nn as nn
+import torch.nn.functional as F
+import copy
 
 class DQN(nn.Module):
 
     def __init__(self, n_observations, n_actions):
         super(DQN, self).__init__()
-        self.layer1 = nn.Linear(n_observations, 128)
-        self.layer2 = nn.Linear(128, 128)
-        self.layer3 = nn.Linear(128, n_actions)
+
+        self.layer1 = nn.Linear(n_observations, 32)
+        self.layer2 = nn.Linear(32, 64)
+        self.layer3 = nn.Linear(64, n_actions)
+
+    def forward(self, states):
+        """Forward pass."""
+        x = F.relu(self.layer1(states))
+        x = F.relu(self.layer2(x))
+        return nn.Tanh()(self.layer3(x))
